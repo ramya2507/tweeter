e9411292b288fb7f89aceceaf2a7a9258487b41e
Ramya Adimoolam <ramyaa1431@gmail.com>
2020-12-15 15:59:03 +0000

    refactored code to use this.sliblings

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('#tweet-text').on('keyup',function(){
     const remainingChar = 140 - $(this).val().length;
-    const remainingCharText = $('#under-new-tweet .counter');
+    const remainingCharText = $(this).siblings('div').children('.counter');
     remainingCharText.text(remainingChar);
     if (remainingChar < 0) {
       remainingCharText.addClass("over-the-limit");
